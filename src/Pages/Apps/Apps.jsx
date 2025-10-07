36e799bf2291e9ce97df964022cc88f76a26bc4e
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2025-10-08 02:49:20 +0600

    after appCards work successfully

--- src/Pages/Apps/Apps.jsx
+++ src/Pages/Apps/Apps.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
+import App from '../App/App';
 
 const Apps = () => {
-    
+    const appsData = useLoaderData()
+    // console.log(appsData)
+
     return (
         <div className='max-w-6xl mx-auto mt-5'>
             <div> 
-                <h1 className='text-3xl md:text-4xl text text-center font-semibold'>Trending Apps</h1>
+                <h1 className='text-3xl md:text-4xl text text-center font-bold'>Trending Apps</h1>
                 <p className='text-gray-600 text-center pt-3 text-sm lg:text-lg '>Explore All Trending Apps on the Market developed by us</p>
             </div>
-            <div> 
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-6'> 
                 {
-
+                    appsData.map(AppData => <App key={AppData.id} AppData={AppData}></App>)
                 }
+            </div>
+            <div className='mt-8 text-center'> 
+                <button className='btn px-8 text-xl  bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white rounded-xl '>Show All</button>
             </div>
         </div>
     );
